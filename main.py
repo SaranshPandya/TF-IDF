568c4c4847452907afa3b0590a6ea34e49aa8505
Saransh Pandya <saranshpandya@Saranshs-MacBook-Pro.local>
2025-01-06 22:15:47 +0530

    Implimented Word-To-Text as lib

---
+++ main.py
@@ -0,0 +1,8 @@
+from utils import WordToIndex
+
+indexer = WordToIndex()
+
+# word = indexer.readtext('006.txt')
+word_to_index = indexer.word_to_index('006.txt')
+
+print(word_to_index)
--- utils.py
+++ utils.py
@@ -9,29 +9,26 @@ and finally implimenting TF-IDF algorithm
 """
 import nltk
 
-def readtext(document_name):
-	with open(document_name, 'r') as file:
-		text = file.read()
-		words = text.split() 
+class WordToIndex:
 
-	return words
+    def readtext(self, document_name):
+        with open(document_name, 'r') as file:
+            text = file.read()
+            words = text.split() 
 
-def word_to_index(documents):
-	current_index = 0
-	word2index = {}
+        return words
 
-	for doc in documents:
-		tokens = nltk.word_tokenize(doc)
-		for token in tokens:
-			if token not in word2index:
-				word2index[token] = current_index
-				current_index += 1
-	
-	return word2index
+    def word_to_index(self, documents):
+        documents = self.readtext(documents)
+        current_index = 0
+        word2index = {}
 
-
-words = readtext('001.txt')
-
-print(word_to_index(words))
-
+        for doc in documents:
+            tokens = nltk.word_tokenize(doc)
+            for token in tokens:
+                if token not in word2index:
+                    word2index[token] = current_index
+                    current_index += 1
+        
+        return word2index
 
